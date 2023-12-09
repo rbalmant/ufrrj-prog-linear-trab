d47f95f1fd0c4642c1c2dbfd7635de083b1d9503
Raphael Balmant Rodrigues Chaves <raphael.balmant@gmail.com>
2023-12-09 00:28:16 -0300

    Enhancement to get more data out of main method

--- code/src/test.py
+++ code/src/test.py
@@ -1,22 +1,59 @@
 from os import path
 import pandas as pd
+import numpy as np
 from train.lpda import lpda, simple_predict, predict
 from data_utils import get_data_folder
 from dataset import heart, meta_heart, diabetes, meta_diabetes, diabetes2, meta_diabetes2
 
-def main(dataset: pd.DataFrame, y_column: str) -> (float, float, float):
+class ModelResult:
+    solution: dict
+    test: dict
+    scores: dict
+
+    def __str__(self):
+        np.set_printoptions(linewidth=np.inf)
+        return f"Solution: \n" \
+                    f"\ta = {str(self.solution['a'])}\n" \
+                    f"\tb = {str(self.solution['b'])}\n" \
+                f"\n\n" \
+                f"Test set: \n" \
+                    f"\tX_test = {str(self.test['X_test'])}\n" \
+                    f"\ty_test = {str(self.test['y_test'])}\n" \
+                f"\n\n" \
+                f"Scores: \n" \
+                    f"\tPrecision: {str(self.scores['precision'])}\n" \
+                    f"\tAccuracy: {str(self.scores['precision'])}\n" \
+                    f"\tF1 score: {str(self.scores['f1'])}\n" \
+                
+
+def main(dataset: pd.DataFrame, y_column: str) -> ModelResult:
     a, b, X_test, y_test = lpda(dataset, y_column)
 
-    # Solution to problem
-    print(a)
-    print(b)
+    solution = {
+        'a': a,
+        'b': b
+    }
+
+    test = {
+        'X_test': X_test,
+        'y_test': y_test
+    }
 
     # Calculate some metrics based on test data
     precision, accuracy, f1 = predict(a, b, X_test, y_test)
-    print(f"Precision: {str(precision)}")
-    print(f"Accuracy: {str(accuracy)}")
-    print(f"F1 Score: {str(f1)}")
 
-    return (precision, accuracy, f1)
+    scores = {
+        'precision': precision,
+        'accuracy': accuracy,
+        'f1': f1
+    }
 
-main(diabetes2, meta_diabetes2.y_column)
+    model_result = ModelResult()
+    model_result.solution = solution
+    model_result.test = test
+    model_result.scores = scores
+
+    return model_result
+
+model_result = main(heart, meta_heart.y_column)
+print(model_result)
