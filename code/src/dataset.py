7c7b0e706a7f6a536e6b3a50ab4ca93359188894
Raphael Balmant Rodrigues Chaves <raphael.balmant@ibm.com>
2023-12-11 18:41:42 -0300

    Remove code reference to dataset not being used anymore

--- code/src/dataset.py
+++ code/src/dataset.py
@@ -20,11 +20,6 @@ datasets_raw = [
         "folder": "diabetes",
         "file": "diabetes_binary_5050split_health_indicators_BRFSS2021.csv",
         "y_column": "Diabetes_binary"
-    },
-    {
-        "folder": "diabetes2",
-        "file": "diabetes_binary_health_indicators_BRFSS2021.csv",
-        "y_column": "Diabetes_binary"
     }
 ]
 
@@ -42,10 +37,3 @@ meta_diabetes.file = datasets_raw[1]['file']
 meta_diabetes.y_column = datasets_raw[1]['y_column']
 
 diabetes = pd.read_csv(path.join(get_data_folder(), meta_diabetes.folder, meta_diabetes.file))
-
-meta_diabetes2: DataSet = DataSet()
-meta_diabetes2.folder = datasets_raw[2]['folder']
-meta_diabetes2.file = datasets_raw[2]['file']
-meta_diabetes2.y_column = datasets_raw[2]['y_column']
-
-diabetes2 = pd.read_csv(path.join(get_data_folder(), meta_diabetes2.folder, meta_diabetes2.file))
